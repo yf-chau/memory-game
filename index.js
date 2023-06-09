091d857f0ef8c77843f340b9203240c4a6f3c194
YF Chau <yaufai.chau@gmail.com>
2023-06-09 11:41:54 +0100

    Updated logic so that coors was passed around between functions, instead of card number.

--- index.js
+++ index.js
@@ -3,23 +3,33 @@ const c = require("ansi-color")
 
 const gridSize = 3 //6*7 = 42 which is the max
 
-let grid = [] // A nested array containing the "cards"
-let card1 = 0
-let card2 = 0
+let grid = [] // A nested array containing the cards
+let cardsCoor = []
 let rightPair = false
+
+
+//Main program
 
 gameSetup(grid, gridSize);
 
 while (true) {
 
     displayGrid(grid)
-    cards = chooseCard()
-    rightPair = showChosenCard(cards)
 
-    if (rightPair) {
-        updateGrid(cards)
-    }
+    cardsCoor = chooseCard()
 
+    rightPair = showChosenCard(cardsCoor[0], cardsCoor[1])
+
+}
+
+
+function convertCoor (cardNumber) {
+
+    cardRow = Math.floor((cardNumber - 1) / (gridSize + 1))
+
+    cardCol = ((cardNumber -1) % (gridSize + 1))
+
+    return [cardRow, cardCol]
 }
 
 function gameSetup(grid, gridSize) {
@@ -53,7 +63,7 @@ function displayGrid(grid) {
             } else {
                 row += ` ${counter.toString().length === 1 ? ' ' + counter : counter} `;
             }
-            
+
             counter++
         }
         console.log(row);
@@ -62,30 +72,39 @@ function displayGrid(grid) {
 }
 
 function chooseCard() {
-    card1 = Number(prompt(`Choose the first card:`, card1))
-    card2 = Number(prompt("Choose the second card:", card2))
+    card1 = Number(prompt(`Choose the first card:`))
 
-    return [card1, card2]
+    card1Coor = convertCoor(card1)    
+
+    console.log ("Card1row: ", card1Coor[0])
+    console.log ("Card1col: ", card1Coor[1])
+
+    card2 = Number(prompt("Choose the second card:"))
+
+    card2Coor = convertCoor(card2)    
+
+    console.log ("Card2row: ", card2Coor[0])
+    console.log ("Card2col: ", card2Coor[1])
+
+    return [card1Coor, card2Coor]
 }
 
-function showChosenCard(cards) {
+function showChosenCard(card1Coor, card2Coor) {
 
     let counter = 1
-    let card1 = ''
-    let card2 = ''
+
+    console.log(card1Coor, card2Coor)
 
     for (let i = 0; i < grid.length; i++) {
         let row = `   `;
         for (let j = 0; j < grid[i].length; j++) {
 
-            if (counter === cards[0]) {
+            if ((i === card1Coor[0] && j === card1Coor[1]) || (i === card2Coor[0] && j === card2Coor[1])) {
                 row += ` ${' ' + grid[i][j]} `;
-                card1 = grid[i][j] //save the value of card 1
-            } else if (counter === cards[1]) {
-                row += ` ${' ' + grid[i][j]} `;
-                card2 = grid[i][j] //save the value of card 2
-            } else if (grid[i][j] === "#") {
-                row += ` ${' #'} `
+            // } else if (counter === cards[1]) {
+            //     row += ` ${' ' + grid[i][j]} `;
+            // } else if (grid[i][j] === "#") {
+            //     row += ` ${' #'} `
             } else {
                 row += ` ${counter.toString().length === 1 ? ' ' + counter : counter} `;
             }
@@ -93,19 +112,33 @@ function showChosenCard(cards) {
         }
         console.log(row);
     }
-    return card1 === card2 //return if card1 equals card 2
+
+    console.log()
+    console.log(card1Coor, card2Coor)
+
+    if (grid[card1Coor[0]][card1Coor[1]] === grid[card2Coor[0]][card2Coor[1]]) {
+
+        console.log("Your are right!")
+        grid[card1Coor[0]][card1Coor[1]] = '#'
+        grid[card2Coor[0]][card2Coor[1]] = '#'
+
+    } else {
+        console.log("Your are wrong! Try again.")
+    }
+
+    //return card1 === card2
     // setTimeout(() => { console.clear() }, 5000)
 }
 
-function updateGrid(cards) {
-    let counter = 1
+// function updateGrid(cards) {
+//     let counter = 1
 
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            if (counter === cards[0] || counter === cards[1]) {
-                grid[i][j] = "#"
-            }
-            counter++
-        }
-    }
-}
+//     for (let i = 0; i < grid.length; i++) {
+//         for (let j = 0; j < grid[i].length; j++) {
+//             if (counter === cards[0] || counter === cards[1]) {
+//                 grid[i][j] = "#"
+//             }
+//             counter++
+//         }
+//     }
+// }
